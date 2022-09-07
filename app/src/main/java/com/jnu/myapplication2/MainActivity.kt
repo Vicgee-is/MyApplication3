1e963dab4959c8661ac241c60e195b794e48b1de
Vicgee-is <wyjdsg0578@163.com>
2022-09-07 10:40:13 +0800

    Initial commit

---
+++ app/src/main/java/com/jnu/myapplication2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.jnu.myapplication2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //TextView text_view_hello_world = (TextView)findViewById(R.id.text_view_hello_world)
+        val text_view_hello_world = findViewById(R.id.text_view_hello_world) as TextView
+        text_view_hello_world.setText("你好Android")
+    }
+}
