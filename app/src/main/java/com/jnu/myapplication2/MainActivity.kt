a6c188621f2d8fcf449e922ce42fab8bd22b6dfd
Vicgee-is <wyjdsg0578@163.com>
2022-09-07 11:02:26 +0800

    first change

--- app/src/main/java/com/jnu/myapplication2/MainActivity.kt
+++ app/src/main/java/com/jnu/myapplication2/MainActivity.kt
@@ -11,5 +11,7 @@ class MainActivity : AppCompatActivity() {
         //TextView text_view_hello_world = (TextView)findViewById(R.id.text_view_hello_world)
         val text_view_hello_world = findViewById(R.id.text_view_hello_world) as TextView
         text_view_hello_world.setText("你好Android")
+        // text_view_hello_world.setText(R.string.hello_world)
+        text_view_hello_world.setText(this.resources.getText(R.string.hello_world).toString())
     }
 }
